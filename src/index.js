40495cc4362486c51f92c8e266de5952e3e11235
Mat Moore <matmoore@users.noreply.github.com>
2020-10-06 16:18:21 +0100

    Wait only for DOM
    The javascript can start manipulating the page as soon as the dom is parsed
    
    https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const wordlist = ["과일", "술", "우산", "잎", "교회", "강", "계절"];
 
 const sample = (items) => items[Math.floor(Math.random() * items.length)];
 
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
   const word = sample(wordlist);
 
   const card = document.querySelector("#card .cardtext");
   const newText = document.createTextNode(word);
   card.appendChild(newText);
-};
+});
